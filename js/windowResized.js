85ee9efcf46ad1fa450720369e8964adeeee2ec2
Tutanrhamun <enviouskirbier@gmail.com>
2016-04-27 23:14:12 +0900

    ややマテリアルUIよりに修正

--- js/windowResized.js
+++ js/windowResized.js
@@ -1,4 +1,4 @@
-var timer = false;
+/*var timer = false;
 $(window).resize(function() {
     if (timer !== false) {
         clearTimeout(timer);
@@ -19,3 +19,17 @@ $(window).resize(function() {
 });
 
 //http://kadoppe.com/archives/2012/02/jquery-window-resize-event.html
+*/
+
+$(window).resize(function(){
+  console.log(window.innerWidth);
+  if(window.innerWidth >= 768){
+    $(function(){
+      $("#menu").css("display", "inline");
+    });
+  }else{
+    $(function(){
+      $("#menu").css("display", "none");
+    });
+  }
+});
